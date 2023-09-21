f234f7a70e07387849cee4788f86485d81abf4fd
Manvi Kishore <kishm01@pfw.edu>
2023-09-21 14:52:09 -0400

    Added boilerplate code

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      <header className="App-header">
+        <nav className ="searchBar">
+          Search bar will come here
+        </nav>
+        </header>
+        </div>
+      <nav>~
+      <ul>
+        <li><a href="">Services</a></li>
+        <li><a href="">University List</a></li>~
+        <li><a href="">Events</a></li>
+        <li><a href="">Aluminni Connect</a></li>
+      </ul>
+    </nav>
+    <aside>
+      <img
+        src= '../images/home_page_image.jpg'
+        width="350"
+        height="400"
+      />
+    </aside>
+  
+    <div class="centered">Your Gateway for Global Education </div>
+    <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
