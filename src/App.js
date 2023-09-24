51067c2f89c2bd1ae1d80a7011bd55b1621f054d
Manvi Kishore <kishm01@pfw.edu>
2023-09-23 21:25:49 -0400

    updated navbar

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
-      <div>
-      <header className="App-header">
-        <nav className ="searchBar">
-          Search bar will come here
-        </nav>
-        </header>
-        </div>
-      <nav>~
-      <ul>
-        <li><a href="">Services</a></li>
-        <li><a href="">University List</a></li>~
-        <li><a href="">Events</a></li>
-        <li><a href="">Aluminni Connect</a></li>
-      </ul>
-    </nav>
-    <aside>
-      <img
-        src= '../images/home_page_image.jpg'
-        width="350"
-        height="400"
-      />
-    </aside>
-  
-    <div class="centered">Your Gateway for Global Education </div>
-    <button>Get Started</button>
+      <Navbar />
     </div>
   );
 }
