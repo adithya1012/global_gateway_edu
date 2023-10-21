1c407dc7a37c5ffd7580875b7998c6c656ec5a1c
Shreyas Bedekar <bedesn01@pfw.edu>
2023-10-21 15:34:55 -0400

    Tile Service Commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Navbar from "./components/navbar";
 import HomeBody from "./components/homeBody";
 import LoginBody from "./components/loginPage";
 import GreService from "./components/greService";
+import TileService from "./components/tileService";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <HomeBody />
+      {/*<Navbar />*/}
+      {/*<HomeBody />*/}
       {/* <LoginBody /> */}
       {/* <GreService /> */}
+      <TileService/>
     </div>
   );
 }
---
+++ src/components/tileService.jsx
@@ -0,0 +1,67 @@
+import React,{Component} from "react";
+import '../css/tileservice.css';
+
+const cardData = [
+    {
+      title: 'Exam Preparation',
+      text: 'Confidently prepare for your GRE and IELTS exams with our comprehensive resources to excel academically and in your career',
+    },
+    {
+      title: 'Letter of Recommendation',
+      text: 'Effortlessly craft personalized and impactful Letters of Recommendation with our user-friendly platform',
+    },
+    {
+      title: 'University Recommendation',
+      text: 'Discover a brighter educational future with tailored university recommendations based on your interests, goals, and preferences',
+    },
+    {
+      title: 'Visa Preparation',
+      text: 'Easily navigate the visa application process with our comprehensive resources and expert guidance',
+    },
+    {
+      title: 'Statement of Purpose',
+      text: 'Achieve your academic and career goals with a compelling Statement of Purpose and expert guidance',
+    },
+    {
+      title: 'Financial Planning',
+      text: 'Empower your financial future with our resources and expert advice, making confident financial decisions for your education and beyond',
+    },
+    {
+      title: 'Flight Booking',
+      text: 'Book affordable flights, compare fares, and secure your journey hassle-free with our trusted travel companion platform.',
+    },
+    {
+      title: 'University Deadline',
+      text: 'Never miss important university application deadlines with our platform informative alerts',
+    },
+    {
+      title: 'Resume Preparation',
+      text: 'Create personalized and impactful resumes with our user-friendly platform. Get comprehensive support for your resume needs.',
+    },
+  ];
+  
+  
+  function Card({ title, text}) {
+    return (
+      <div className="card" style={{ width: '18rem' }}>
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{text}</p>
+        </div>
+      </div>
+    );
+  }
+  
+class TileService extends Component {
+    render() {
+        return (
+            <div className="grid-container">
+            {cardData.map((card, index) => (
+              <Card key={index} {...card} />
+            ))}
+          </div>
+        );
+    }
+}
+
+export default TileService;
