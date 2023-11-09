3ba21426b9b42c5de6c2953ca143d69446380ab1
Manvi Kishore <kishm01@pfw.edu>
2023-11-08 23:02:12 -0500

    added base code for back-end

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,43 +1,39 @@
 import { useState } from "react";
-import { MongoClient } from "mongodb";
+import Axios from "axios";
 
 function Users() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [user, setUser] = useState("");
-  //   const remoteurl =
-  //     "mongodb+srv://manvikishore7:%23123%23Manvi@cluster0.xbak0ji.mongodb.net/?retryWrites=true&w=majority";
-  //   client = new MongoClient(remoteurl);
-
-  //   const handleOnSubmit = async (e) => {
-  //     const query = { name: "Manvi" };
-  //     const result = await client
-  //       .db("GGE_DB")
-  //       .collection("users")
-  //       .findOne(query)
-  //       .toArray();
-
-  //     setUser(result);
-  //   };
-
+  const handleClick = (user) => {
+    let result = Axios.post("http://localhost:8000/add_user", {
+      name: name,
+      email: email,
+    });
+    result = result.json();
+    console.warn(result);
+    if (result) {
+      alert("Data saved succesfully");
+      setEmail("");
+      setName("");
+    }
+  };
   return (
     <>
-      <h1>This is React WebApp </h1>
-      <h2>{user}</h2>
+      <h1>Add User </h1>
       <form action="">
         <input
           type="text"
           placeholder="name"
           value={name}
-          // onChange={(e) => setName(e.target.value)}
+          onChange={(e) => setName(e.target.value)}
         />
         <input
           type="email"
           placeholder="email"
           value={email}
-          //onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)}
         />
-        <button type="submit" onClick={handleOnSubmit}>
+        <button type="submit" onClick={handleClick}>
           submit
         </button>
       </form>
