4ab91a089728313e9eeafd7c6d13f75e7a397383
master <kunasinduja1998@gmail.com>
2023-11-22 13:24:07 -0500

    resolving merge conflicts

--- back-end/models.js
+++ back-end/models.js
@@ -8,13 +8,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-<<<<<<< HEAD
-=======
   password: {
     type: String,
     required: true,
   }
->>>>>>> 51820d7c60ae06b1e21cfc6b3bdaad5dc59a98be
 });
 
 const User = mongoose.model("User", UserSchema);
